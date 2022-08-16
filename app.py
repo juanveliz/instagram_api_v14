4a5b03d876ca070406ed9bbdcc0efff483c37943
Juan Veliz <juan.veliz.godoy@gmail.com>
2022-08-15 22:06:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@
 #   Created:   06.08.2021
 #   Software bajo licencia GPLv3.
 #   API Graph de Instagram version: v14.0. ref: https://developers.facebook.com/docs/instagram-basic-display-api/changelog
-#   Notion version : 2022-06-28. ref: https://developers.notion.com/reference/versioning
-#   Google Sheets API : ref:https://developers.google.com/sheets/api/quickstart/python
 #
 #   This python application allows to automate Instagram posting using the official API.
 # 
